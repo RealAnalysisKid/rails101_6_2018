3e57f3153c12bcec83fd6a72b0ce583eb2034af1
Xin <Detective_boys@hotmil.com>
2019-01-03 12:51:55 +0800

    implement post#edit&destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+  before_action :find_post_and_check_permissin, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
   end
 
   def create
@@ -19,7 +23,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to account_posts_path, alert: "Post Deleted"
+    end
+  end
+
   private
+
+  def find_post_and_check_permissin
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You Have No Permission"
+    end
+  end
+
   def post_params
     params.require(:post).permit(:content)
   end
